460a9f590e6d9e304eec75825b1a7729a48fd541
Le Tran <LTran.1231@gmail.com>
2015-08-19 16:27:17 -0700

    remove allow blank in the user password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,6 @@ class User < ActiveRecord::Base
                     
   has_secure_password 
 
-  validates :password, length: { minimum: 6 }, allow_blank: true
+  validates :password, length: { minimum: 6 }
 
 end
