96881a82d87fddda053a67e3fb4e5c7fb60005df
Le Tran <LTran.1231@gmail.com>
2015-08-20 16:43:51 -0700

    pins are clickable with post id as a popup

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,6 @@
 
 $(function(){
-  // loginWithFacebook();
+
   firebase = "https://pinasyougo.firebaseio.com/";
   fbPostsRef = new Firebase(firebase + "posts/");
 
@@ -14,85 +14,79 @@ $(function(){
   submitPost.contentEditor('.summernote-post-editor');
   submitPost.deletePin('.delete_location');
   
-  sendPostsCoordsToFB();
-  
   L.mapbox.accessToken = "pk.eyJ1IjoibHRyYW4xMjMxIiwiYSI6IjJhNThiNDcxZDczNWQwZTkwNjMxMThhNDE4ZGUyNTA2In0.obLVCvFCcLLDKdV0liwQRQ";
-  map = L.mapbox.map(document.getElementById("map"), "ltran1231.mmfe4jdj", {
-    zoomControl: false
-  }).setView([45.706, 11.953], 2);
-  
-  map.dragging.disable();
-  map.touchZoom.disable();
-  map.doubleClickZoom.disable();
-  map.scrollWheelZoom.disable();
+  pinItMapBox = "ltran1231.mmfe4jdj"
 
-  setMarkers();
+  map = L.mapbox.map(document.getElementById("map"), pinItMapBox, {
+    // zoomControl: false
+  }).setView([45.706, 11.953], 2)
+
+  
+  // map.dragging.disable();
+  // map.touchZoom.disable();
+  // // map.doubleClickZoom.disable();
+  // map.scrollWheelZoom.disable();
+
+  Map.sendPostsCoordsToFB('/posts_data');
+  Map.setMarkers();
 
   // $('.carousel').carousel({
   //     interval: 10000 //changes the speed
   // })
 });
 
+var Map = (function(){
 
-var sendPostsCoordsToFB = function () {
-  $.get('/posts_data').done(function (pinsData) {
-    console.log(pinsData);
-    // send data to firebase storage
-    // var list_coords = posts.reduce(function(object, coords, index) {
-    //   object[index] = JSON.parse("[" + coords.join() + "]");
-    //   return object;
-    // }, {});
-    // console.log(list_coords)
-    // remove existing coords and add list
-    fbPostsRef.remove();
-    var firebaseCoords = fbPostsRef.set(pinsData);
-
-    // console.log(firebaseCoords)
+  var sendPostsCoordsToFB = (function (getRoute) {
+    $.get(getRoute).done(function (pinsData) {
+      fbPostsRef.remove();
+      var firebaseCoords = fbPostsRef.set(pinsData);
+    })
   });
-}
+
+  var setMarkers = (function() {
+    var count = 0;
+    fbPostsRef.on("child_added", function(snap) {
+      count++;
+      // console.log("added", snap.key());
+  var markers = new L.MarkerClusterGroup();
 
 
+      for(var k in snap.val()){
+        var color = '#' + [
+        (~~(Math.random() * 16)).toString(16),
+        (~~(Math.random() * 16)).toString(16),
+        (~~(Math.random() * 16)).toString(16)].join('');
+        var title = k;
 
-var setMarkers = function() {
-  var count = 0;
-  fbPostsRef.on("child_added", function(snap) {
-    count++;
-    // console.log("added", snap.key());
-    for(var k in snap.val()){
-      color = '#' + [
-      (~~(Math.random() * 16)).toString(16),
-      (~~(Math.random() * 16)).toString(16),
-      (~~(Math.random() * 16)).toString(16)].join('');
-
-      var postion = snap.val()[k].coords;
-      var marker = L.marker(postion, {
-        draggable: false,
-        icon: L.mapbox.marker.icon({
-          'marker-color': color
+        var postion = snap.val()[k].coords;
+        var marker = L.marker(postion, {
+          draggable: false,
+          icon: L.mapbox.marker.icon({
+            'marker-symbol': 'post',
+            'marker-color': color
+          }),
+          title: title
         })
-      }).addTo(map)
-    }  
+
+        marker.bindPopup(title);
+        markers.addLayer(marker);
+
+      }
+      map.addLayer(markers);  
+    })
   });
 
-  // fbPostsRef.on('child_added', function(snapshot){
-  //   // var features = []
-  //   console.log(snapshot);
+  return {
+    sendPostsCoordsToFB: sendPostsCoordsToFB,
+    setMarkers: setMarkers,
+    // featureLayer: featureLayer
+  }
+})();
 
-  //   for (var k in snapshot.val()) {
-  //     // generate random color for the marker
-      
-  //     var postID = snapshot.val()  
-  //     console.log(postID)
-  //     var post = snapshot.val()  
-  //     console.log(post)
-  //     var postion = snapshot.val()[k];
-  //     var marker = L.marker(postion, {
-  //       draggable: true,
-  //       icon: L.mapbox.marker.icon({
-  //         'marker-color': color
-  //       })
-  //     }).addTo(map)
-  //   }
 
-  // });
-}
+
+
+
+
+
